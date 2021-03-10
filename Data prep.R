09c3f02293689e90a5b4bfed4790dd0f7eafc3e6
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2021-03-10 11:54:02 -0800

    Made a working first version of the app

--- Data prep.R
+++ Data prep.R
@@ -6,44 +6,6 @@ require(discretewq)
 require(readr)
 require(tidyr)
 
-# Bivalves and phytoplankton ----------------------------------------------
-
-
-Data<-DeltaDater(Start_year=1950, End_year=2021, 
-                 Variables = c("Bivalves", "Phytoplankton"),
-                 Regions=NULL, Phyt_start = 1950)
-
-## Bivalves ----------------------------------------------------------------
-
-
-Bivalves<-Data$Bivalves%>%
-  filter(!is.na(Latitude) & !is.na(Longitude))%>%
-  select(Date, Station, Source, Latitude, Longitude)%>%
-  separate(Station, into=c("Station", "Station2"), sep="-")%>%
-  mutate(Year=year(Date),
-         Station2=paste(Station, Station2, sep="-"))%>%
-  group_by(Year, Station, Station2, Source)%>%
-  summarise(Latitude=unique(Latitude), Longitude=unique(Longitude), N=n(), .groups="drop")%>%
-  mutate(Parameter="Bivalves")
-
-## Phytoplankton ----------------------------------------------------------------
-
-
-Phytoplankton<-Data$Phytoplankton%>%
-  filter(!is.na(Latitude) & !is.na(Longitude))%>%
-  select(Date, Station, Source, Latitude, Longitude)%>%
-  mutate(Year=year(Date),
-         Station=case_when(
-           str_detect(Station, "EZ6 SJR") ~ "EZ6 SJR",
-           str_detect(Station, "EZ2 SJR") ~ "EZ2 SJR",
-           str_detect(Station, "EZ2") ~ "EZ2",
-           str_detect(Station, "EZ6") ~ "EZ6",
-           TRUE ~ Station),
-         Station2=if_else(str_detect(Station, "EZ"), paste(Station, Date), Station))%>%
-  group_by(Year, Station, Station2, Source)%>%
-  summarise(Latitude=unique(Latitude), Longitude=unique(Longitude), N=n(), .groups="drop")%>%
-  mutate(Parameter="Phytoplankton")
-
 
 # Zooplankton -------------------------------------------------------------
 
@@ -71,6 +33,21 @@ YBFMP_zoop<-read_csv(file.path(tempdir(), "YBFMP_zoop.csv"),
   left_join(YBFMP_zoop_stations, by="Station")%>%
   filter(!is.na(Latitude) & !is.na(Longitude))%>%
   mutate(Station2=Station)
+
+
+# FRP ---------------------------------------------------------------------
+
+FRP_stations<-st_read("FRPstations")
+
+download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.269.2&entityid=41d077ac60682ce93c57c1ae1957fdcb", file.path(tempdir(), "FRP_Fish.csv"), mode="wb")
+download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.269.2&entityid=630f16b33a9cbf75f1989fc18690a6b3", file.path(tempdir(), "FRP_bugs.csv"), mode="wb")
+download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.269.2&entityid=d4c76f209a0653aa86bab1ff93ab9853", file.path(tempdir(), "FRP_zoops.csv"), mode="wb")
+
+FRP_zoop<-read_csv(file.path(tempdir(), "FRP_zoops.csv"),
+                   col_types=cols_only(Date="c", Time="c", StationCode="c"))
+
+FRP_bugs<-read_csv(file.path(tempdir(), "FRP_bugs.csv"))
+##Use st_nearest_feature, not st_join to assign FRP stations
 
 ## All zoops ---------------------------------------------------------------
 
@@ -197,11 +174,128 @@ Fish<-WQ%>%
   bind_rows(YBFMP_fish)%>%
   mutate(Parameter="Fish")
 
+# Benthic and phytoplankton ----------------------------------------------
+
+
+Data<-DeltaDater(Start_year=1950, End_year=2021, 
+                 Variables = c("Bivalves", "Phytoplankton"),
+                 Regions=NULL, Phyt_start = 1950)
+
+## Benthic ----------------------------------------------------------------
+
+## Removing the L, R, and C stations and just combining them into 1 summary statistic
+
+Benthic<-Data$Bivalves%>%
+  select(Date, Station, Source, Latitude, Longitude)%>%
+  separate(Station, into=c("Station", "Station2"), sep="-")%>%
+  mutate(Year=year(Date),
+         Station2=Station)%>%
+  group_by(Year, Station, Station2, Source)%>%
+  summarise(N=n(), .groups="drop")%>%
+  mutate(Parameter="Benthic")%>%
+  left_join(WQ%>%
+              filter(Source=="EMP")%>%
+              select(Station, Latitude, Longitude)%>%
+              distinct(),
+            by="Station")
+
+## Phytoplankton ----------------------------------------------------------------
+
+
+Phytoplankton<-Data$Phytoplankton%>%
+  select(Date, Station, Source, Latitude, Longitude)%>%
+  mutate(Year=year(Date),
+         Station=case_when(
+           str_detect(Station, "EZ6 SJR") ~ "EZ6 SJR",
+           str_detect(Station, "EZ2 SJR") ~ "EZ2 SJR",
+           str_detect(Station, "EZ2") ~ "EZ2",
+           str_detect(Station, "EZ6") ~ "EZ6",
+           TRUE ~ Station),
+         Station2=if_else(str_detect(Station, "EZ"), paste(Station, Date), Station))%>%
+  group_by(Year, Station, Station2, Source)%>%
+  summarise(N=n(), .groups="drop")%>%
+  mutate(Parameter="Phytoplankton")%>%
+  left_join(WQ%>%
+              filter(Source=="EMP")%>%
+              select(Station, Station2, Latitude, Longitude)%>%
+              distinct(),
+            by=c("Station", "Station2"))%>%
+  filter(!is.na(Latitude) & !is.na(Longitude))
+
+# Match EMP zoop to WQ stations -------------------------------------------
+
+zoop_stations<-zooper::stations%>%
+  filter(Source=="EMP" & !is.na(Latitude) & !is.na(Longitude))%>%
+  st_as_sf(coords=c("Longitude", "Latitude"), crs=4326, remove=F)%>%
+  st_transform(crs=26910)
+
+WQ_stations<-WQ%>%
+  filter(Source=="EMP" & !str_detect(Station, "EZ"))%>%
+  distinct(Station, Latitude, Longitude)%>%
+  st_as_sf(coords=c("Longitude", "Latitude"), crs=4326, remove=F)%>%
+  st_transform(crs=26910)
+
+match<-st_nearest_feature(zoop_stations, WQ_stations)
+
+dist<-st_distance(zoop_stations, WQ_stations[match,], by_element=T)
+
+zoop_stations_match<-zoop_stations%>%
+  mutate(WQ_station=WQ_stations$Station[match],
+         Distance=as.numeric(dist))%>%
+  st_drop_geometry()%>%
+  left_join(WQ_stations%>%
+              st_drop_geometry()%>%
+              select(WQ_lat=Latitude, WQ_lon=Longitude, Station),
+            by=c("WQ_station"="Station"))
+
+
+ggplot(data=filter(zoop_stations_match, Distance<=1000))+
+  #geom_sf(data=deltamapr::WW_Delta%>%st_transform(crs=4326))+
+  geom_point(aes(y=Latitude, x=Longitude, color=Distance), shape=16)+
+  geom_point(aes(y=WQ_lat, x=WQ_lon, color=Distance), shape=17)+
+  geom_segment(aes(y=Latitude, x=Longitude, yend=WQ_lat, xend=WQ_lon))+
+  scale_color_viridis_c()+
+  theme_bw()
+
+zoop_stations_match_final<-zoop_stations_match%>%
+  filter(Distance<=1000)%>%
+  mutate(Source="EMP")
+
+Zoop_final<-Zoop%>%
+  left_join(zoop_stations_match_final%>%
+              select(Source, Station, WQ_station, WQ_lat, WQ_lon),
+            by=c("Source", "Station"))%>%
+  mutate(Station=if_else(is.na(WQ_station), Station, WQ_station),
+         Station2=if_else(is.na(WQ_station), Station2, WQ_station),
+         Latitude=if_else(is.na(WQ_station), Latitude, WQ_lat),
+         Longitude=if_else(is.na(WQ_station), Longitude, WQ_lon))%>%
+  select(-WQ_station, -WQ_lat, -WQ_lon)
+
+
+# Create master list of other EMP station names ---------------------------
+
+zoop_stations_final<-zoop_stations_match_final%>%
+  select(Station, WQ_station)%>%
+  distinct()%>%
+  rename(Zoop_station=Station, Station=WQ_station)
+
+Benthic_stations<-Data$Bivalves%>%
+  select(Station)%>%
+  distinct()%>%
+  separate(Station, into=c("Station", "Benthic_station"), sep="-")%>%
+  mutate(Benthic_station=paste(Station, Benthic_station, sep="-"))%>%
+  group_by(Station)%>%
+  summarise(Benthic_station=list(unique(Benthic_station)))
+
+BenZoop_stations<-full_join(zoop_stations_final, Benthic_stations,
+               by="Station")
 
 # Merge everything --------------------------------------------------------
 
-Stations<-bind_rows(Bivalves, Phytoplankton, Zoop, WQ, Fish)%>%
-  mutate(StationID=paste(Source, Station2))
+Stations<-bind_rows(Benthic, Phytoplankton, Zoop_final, WQ, Fish)%>%
+  mutate(Source=recode(Source, twentymm="20mm"),
+         Station=recode(Station, NZEZ2="EZ2", NZEZ6="EZ6", NZEZ2SJR="EZ2-SJR", NZEZ6SJR="EZ6-SJR"),
+         StationID=paste(Source, Station2))
 
 Multiples<-Stations%>%
   group_by(StationID)%>%
@@ -226,5 +320,18 @@ ggplot()+
 Stations_final<-Stations%>%
   group_by(StationID)%>%
   mutate(Latitude=mean(Latitude), Longitude=mean(Longitude))%>%
+  ungroup()%>%
   pivot_wider(names_from = Parameter, values_from=N)%>%
-  mutate(across(c(Bivalves, Phytoplankton, Zooplankton, Water_quality, Fish), ~replace_na(.x, 0)))
+  mutate(across(c(Benthic, Phytoplankton, Zooplankton, Water_quality, Fish), ~replace_na(.x, 0)),
+         N_total=Benthic + Phytoplankton + Zooplankton + Water_quality + Fish)%>%
+  left_join(BenZoop_stations, by="Station")%>%
+  rowwise()%>%
+  mutate(Extra_stations=case_when(
+    is.null(unlist(Benthic_station)) & is.na(Zoop_station) ~ NA_character_,
+    !is.null(unlist(Benthic_station)) & is.na(Zoop_station) ~ paste0("Benthic: ", paste0(unlist(Benthic_station), collapse=", ")),
+    is.null(unlist(Benthic_station)) & !is.na(Zoop_station) ~ paste0("Zooplankton: ", Zoop_station),
+    TRUE ~ paste0(paste0("Benthic: ", paste0(unlist(Benthic_station), collapse=", ")), " | ", paste0("Zooplankton: ", Zoop_station))
+  ))%>%
+  ungroup()
+
+saveRDS(Stations_final, file="app/Sampling.Rds")
--- app/app.R
+++ app/app.R
@@ -1,37 +1,169 @@
 
 library(shiny)
+library(dplyr)
+library(sf)
 library(leaflet)
+require(shinyWidgets)
+require(rlang)
+require(stringr)
 
-
+Sampling<-readRDS("Sampling.Rds")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("IEP stations map", id="nav",
+                 tabPanel("Info", 
+                          "..."
+                 ),
                  
-                 tabPanel("Interactive map",
-                          div(class="outer",
+                 tabPanel("Interactive map", value="map",
+                          
                               
                               # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                              leafletOutput("map", width="100", height="100"),
+                              leafletOutput("mapplot", width="100%", height="100vh"),
                               
                               # Shiny versions prior to 0.11 should use class = "modal" instead.
                               absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                                             draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
                                             width = 330, height = "auto",
-                                            ...)
-                          )
-                 ),
-                 tabPanel("Info", 
-                          ...
+                                            uiOutput("Surveys"),
+                                            uiOutput("Parameters"),
+                                            prettySwitch("Exclude_unfixed", "Exclude unfixed EMP EZ stations?", status = "success", fill = TRUE, bigger=TRUE),
+                                            prettySwitch("Years", "Inspect sampling effort for each year?", status = "success", fill = TRUE, bigger=TRUE),
+                                            conditionalPanel(condition="input.Years",
+                                                             sliderInput("Year", "Select year",
+                                                                         min=min(Sampling$Year), max=max(Sampling$Year), value=2017, step=1, sep="")),
+                                            radioGroupButtons(
+                                                inputId = "Legend",
+                                                label = "Variable for color scale", 
+                                                choices = c("Survey", "Sampling effort"),
+                                                status = "primary"
+                                            )
+                                            )
+                          
                  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+    
+    output$Parameters<-renderUI({
+        rep(input$nav=="map")
+        if(is.null(input$Surveys)){
+            Surveys<-unique(Sampling$Source)
+        } else{
+            Surveys<-input$Surveys
+        }
+        Parameters<-Sampling%>%
+            filter(Source%in%Surveys)%>%
+            select(Benthic, Phytoplankton, Zooplankton, Water_quality, Fish)%>%
+            select(where(~any(.x>0)))%>%
+            names()%>%
+            sort()
+        names(Parameters)<-str_replace(Parameters, "_", " ")
+        pickerInput("Parameters", "Parameters", choices=Parameters, 
+                    selected = Parameters, multiple = T, options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3"))
+    })
+    
+    output$Surveys<-renderUI({
+        rep(input$nav=="map")
+        if(is.null(input$Parameters)){
+            Parameters<-c("Benthic", "Phytoplankton", "Zooplankton", "Water_quality", "Fish")
+        } else{
+            Parameters<-input$Parameters
+        }
+        Surveys<-Sampling%>%
+            filter(if_any(all_of(Parameters), ~.x>0))%>%
+            pull(Source)%>%
+            unique()%>%
+            sort()
+        pickerInput("Surveys", "Surveys", choices=Surveys, 
+                    selected = Surveys, multiple = T, options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3"))
+    })
+    
+    Data<-reactive({
+        req(input$nav=="map")
+        
+        if(input$Exclude_unfixed){
+            out<-filter(Sampling, !Station%in%c("EZ2", "EZ6", "EZ2-SJR", "EZ6-SJR"))
+        }else{
+            out<-Sampling
+        }
+        
+        if(input$Years){
+            str_model <- paste0("<tr><td>Year &nbsp</td><td>%s</td></tr>",
+                                "<tr><td>Survey &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>Station &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>Alternate station names &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Benthic &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Fish &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Phytoplankton &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Water_quality &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Zooplankton &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Total &nbsp</td><td>%s</td></tr>")
+        }else{
+            str_model <- paste0("<tr><td>Survey &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>Station &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>Alternate station names &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Benthic &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Fish &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Phytoplankton &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Water_quality &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Zooplankton &nbsp</td><td>%s</td></tr>", 
+                                "<tr><td>N_Total &nbsp</td><td>%s</td></tr>")
+        }
 
-    output$map <- renderLeaflet({
+        
+        if(input$Years){
+            req(input$Year)
+            out%>%
+                filter(Year==input$Year & Source%in%input$Surveys & if_any(all_of(input$Parameters), ~.x>0))%>%
+                mutate(tooltip=sprintf(str_model, Year, Source, Station, if_else(is.na(Extra_stations), "None", Extra_stations), Benthic, Fish, Phytoplankton, Water_quality, Zooplankton, N_total),
+                       tooltip=paste0( "<table>", tooltip, "</table>" ))
+        }else{
+            out%>%
+                group_by(Station, Station2, Extra_stations, Source, Latitude, Longitude, Zoop_station, Benthic_station)%>%
+                summarise(across(c(Benthic, Phytoplankton, Zooplankton, Water_quality, Fish, N_total), ~sum(.x)), .groups="drop")%>%
+                filter(Source%in%input$Surveys & if_any(all_of(input$Parameters), ~.x>0))%>%
+                mutate(tooltip=sprintf(str_model, Source, Station, if_else(is.na(Extra_stations), "None", Extra_stations), Benthic, Fish, Phytoplankton, Water_quality, Zooplankton, N_total),
+                       tooltip=paste0( "<table>", tooltip, "</table>" ))
+        }
+        
+    })
+    
+  #  pal_survey<-reactive({
+ #       colorFactor(brewer.pal(7, "Dark2"), mapdatataxa()%>%pull(Taxlifestage))
+   # })
+    
+   # pal_effort<-reactive({
+   #     
+   # })
+    
+    tooltip<-reactive({
+        
+    })
+    
+    mapplot<-reactive({
+        req(input$nav=="map")
         leaflet()%>%
             addProviderTiles("Esri.WorldGrayCanvas")%>%
-            fitBounds(~min(Longitude, na.rm=T), ~min(Latitude, na.rm=T), ~max(Longitude, na.rm=T), ~max(Latitude, na.rm=T))
+            fitBounds(min(Sampling$Longitude, na.rm=T), min(Sampling$Latitude), max(Sampling$Longitude), max(Sampling$Latitude))
+    })
+    
+    observe({
+        req(input$nav=="map")
+        #if(input$Legend=="Survey"){
+        #    pal<-pal_survey()
+        #}else{
+        #    pal<-pal_effort()
+        #}
+        mapplot<-leafletProxy("mapplot", session, data = Data())%>%
+            clearMarkers() %>%
+            addCircleMarkers(weight = 1, lng = ~Longitude, lat = ~Latitude,
+                             fillColor = "yellow", color="black", fillOpacity = 0.7, popup=lapply(Data()$tooltip, htmltools::HTML))
+    })
+
+    output$mapplot <- renderLeaflet({
+        mapplot()
             
     })
 }
